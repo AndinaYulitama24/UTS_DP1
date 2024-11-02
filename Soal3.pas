2e1f94b97a69f945c76748004bc44d2a734baf24
Andina_YL <andinayulitama@gmail.com>
2024-11-02 23:22:47 +0700

    Update Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,27 @@
-//ketik kode jawaban noo 3 kamu disini
+program daspro ;
+uses crt ;
+
+var
+n,i,j : integer;
+
+begin
+    clrscr;
+        {Masukan input yang diinginkan}
+    write('masukan jumlah berisi :'); readln(n);
+        {proses penggulanga5n}
+    for i := 1 to n do
+    begin
+    for j := 1 to n do
+        begin
+         if(j = i) or (j =(n-i+1)) then
+            begin
+               write('*'); 
+          {untuk memberhentikan penggulanga} 
+            end
+         else
+            write(' ');  
+        end;
+        {output hasil}
+       writeln;
+    end;
+end.
