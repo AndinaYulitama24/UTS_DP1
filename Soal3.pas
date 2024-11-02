a4580e74bcecc1ef633f21f45ffcec936d7d5c97
Andina_YL <andinayulitama@gmail.com>
2024-11-03 01:38:41 +0700

    Update Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -5,23 +5,22 @@ var
 n,i,j : integer;
 
 begin
-    clrscr;
-        {Masukan input yang diinginkan}
-    write('masukan jumlah berisi :'); readln(n);
-        {proses penggulanga5n}
-    for i := 1 to n do
-    begin
-    for j := 1 to n do
-        begin
-         if(j = i) or (j =(n-i+1)) then
-            begin
-               write('*'); 
-          {untuk memberhentikan penggulanga} 
-            end
+   clrscr; 
+   {Input jumlah baris}
+   write('n = '); readln(n);
+   {Loop untuk membuat baris}
+   for i := 1 to n do
+   begin
+      {Loop untuk membuat kolom}
+      for j := 1 to n do
+      begin
+         if (j = i) or (j = (n-i+1)) then
+            write('*')
+         {Jika bukan posisi bintang, cetak spasi}
          else
-            write(' ');  
-        end;
-        {output hasil}
-       writeln;
-    end;
+            write(' ');
+      end;
+      {Pindah ke baris baru setelah selesai satu baris}
+      writeln;
+   end;
 end.
